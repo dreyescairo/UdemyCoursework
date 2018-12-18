53d00bcb9af59cdf6d77ed847d18b59412e3b0da
David Reyes-Cairo <dreyescairo@gmail.com>
2018-12-17 18:07:22 -0700

    setup debug and config packages.

--- VidlyNodeServices/index.js
+++ VidlyNodeServices/index.js
@@ -1,3 +1,13 @@
+//I rely on these to be set. set them at the very beginning here for now.
+//maybe use dotenv to keep track of these locally
+process.env.APP_PASSWORD = 1234;
+process.env.NODE_ENV = 'development';
+process.env.DEBUG = 'app:startup';
+
+const startupDebugger = require('debug')('app:startup');//app:startup is the value of the env variable DEBUG.
+const dbDebugger = require('debug')('app:db');
+
+const config = require('config'); //the config module for handling different dev environments.
 const Joi = require("joi");
 const logger = require("./logger");
 const express = require("express");
@@ -5,10 +15,17 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 let app = express();
 
-//process.env.NODE_ENV = 'production';
+
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 //get the env using app
 console.log(`app: ${app.get("env")}`);
+
+//configuration
+//the properties we are getting come from the file in the config folder based on the env variable. ie: if development it will read from development.json
+console.log(`Application Name: ${config.get('name')}`);//use config.get() to get the value of a property by name. these properties come from the config folder
+console.log(`Mail Server: ${config.get('mail.host')}`);//use config.get() to get the value of a property by name. these properties come from the config folder
+
+console.log(`Mail Server Password: ${config.get('mail.password')}`);//use config.get() to get the value of a property by name. these properties come from the config folder
 
 //**MIDDLEWARE**//
 //parse body of request
@@ -20,8 +37,11 @@ app.use(helmet());
 if (app.get("env") === "development") {
   //morgan loggs all http requests to the console by default
   app.use(morgan("tiny"));
-  console.log("Morgan enabled...");
+  startupDebugger("Morgan enabled...");
 }
+
+//example of using dbDebugger for dbwork
+dbDebugger('connected to the database...');
 
 //custom midleware example from logger.js
 app.use(logger);
